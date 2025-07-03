a5024553572ae18b7e1ada08ed54993a29b3d276
Z3RO4l <zordzraplha@gmail.com>
2025-07-03 19:33:01 +0000

    teste nota

---
+++ testenota.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main (){
+
+int nota;
+
+ printf("digite a  nota: \n");
+ scanf("%d", &nota);
+
+ if (nota >= 90 ){
+printf("conceito é A  \n");
+ }
+ else if (nota >= 50){
+    printf("conceito é B \n");
+ }else if (nota >= 60) {
+    printf("conceito é C \n");
+ }else if (nota >= 70){
+    printf(" O Conceito é D \n");
+ }else if (nota >= 80) {
+    printf(" o conceito é E\n");
+ }
+ 
+ 
+ 
+ 
+
+
+
+
+
+}
